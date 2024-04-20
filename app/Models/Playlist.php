66575381880109f7ffe45f7dbb8c8699f375b13a
yohashinoio <shinodeveloper1@gmail.com>
2024-04-20 23:42:47 +0900

    Create playlist migration, controller and model

--- app/Models/Playlist.php
+++ app/Models/Playlist.php
@@ -5,12 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Room extends Model
+class Playlist extends Model
 {
     use HasFactory;
-
-    public function playlist()
-    {
-        return $this->hasOne(Playlist::class);
-    }
 }
